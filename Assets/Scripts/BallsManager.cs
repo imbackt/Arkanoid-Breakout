2a17330bd1c69ee1811662e7c7ecc5050e588e74
Ali AlRasheed <ali.alrasheed@outlook.com>
2021-07-04 19:53:45 +0300

    feat: implementing ball movement

---
+++ Assets/Scripts/BallsManager.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallsManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static BallsManager _instance;
+
+    public static BallsManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    #endregion
+
+    [SerializeField]
+    private Ball ballPrefab;
+
+    private Ball initialBall;
+
+    private Rigidbody2D initialBallRb;
+
+    public float initialBallSpeed = 250;
+
+    public List<Ball> Balls { get; set; }
+
+    private void Start()
+    {
+        initBall();
+    }
+
+    private void Update()
+    {
+        if (!GameManager.Instance.isGameStarted)
+        {
+            Vector3 paddlePosition = Paddle.Instance.gameObject.transform.position;
+            Vector3 ballPosition = new Vector3(paddlePosition.x, paddlePosition.y + 0.27f, 0f);
+            initialBall.transform.position = ballPosition;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                initialBallRb.isKinematic = false;
+                initialBallRb.AddForce(new Vector2(0f, initialBallSpeed));
+                GameManager.Instance.isGameStarted = true;
+            }
+        }
+    }
+
+    private void initBall()
+    {
+        Vector3 paddlePosition = Paddle.Instance.gameObject.transform.position;
+        Vector3 startingPosition = new Vector3(paddlePosition.x, paddlePosition.y + 0.27f, 0f);
+        initialBall = Instantiate(ballPrefab, startingPosition, Quaternion.identity);
+        initialBallRb = initialBall.GetComponent<Rigidbody2D>();
+
+        this.Balls = new List<Ball>
+        {
+            initialBall
+        };
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    #region Singleton
+
+    private static Paddle _instance;
+
+    public static Paddle Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    #endregion
+
     private Camera mainCamera;
     private SpriteRenderer sr;
     private float paddleInitialY;
@@ -32,5 +52,28 @@ public class Paddle : MonoBehaviour
         float mousePositionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
         float mouseWorldPositionX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
         this.transform.position = new Vector3(mouseWorldPositionX, paddleInitialY, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            Rigidbody2D ballRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            Vector3 hitPoint = collision.contacts[0].point;
+            Vector2 paddleCenter = new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+
+            ballRb.velocity = Vector2.zero;
+
+            float difference = paddleCenter.x = hitPoint.x;
+
+            if (hitPoint.x < paddleCenter.x)
+            {
+                ballRb.AddForce(new Vector2(-(Mathf.Abs(difference * 200)), BallsManager.Instance.initialBallSpeed));
+            }
+            else
+            {
+                ballRb.AddForce(new Vector2((Mathf.Abs(difference * 200)), BallsManager.Instance.initialBallSpeed));
+            }
+        }
     }
 }
